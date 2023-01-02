f96e5e892bde0e3bfbd35c84e2e2c5ce718e8e6e
Orlando Jimenez <orlando.jmz13@gmail.com>
2023-01-02 13:58:14 -0500

    Se agrega nuevo test para comprobar que el orden en los sumandos no altera el resultado.

--- test_supercalculadora.py
+++ test_supercalculadora.py
@@ -15,5 +15,8 @@ class TestsSupercalculadora( unittest.TestCase):
     def test_sumar_5_y_7(self):
         self.failUnlessEqual(12, self.calc.sumar(5, 7))
 
+    def test_sumar_propiedad_conmutativa(self):
+        self.failUnlessEqual(self.calc.sumar(5, 7), self.calc.sumar(7, 5))
+
 if __name__ == "__main__":
     unittest.main()
