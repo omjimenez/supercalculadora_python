6f6b495415671a1e7b578d7ae30eeec26572813d
Orlando Jimenez <orlando.jmz13@gmail.com>
2023-01-02 14:12:30 -0500

    Se agrega nuevo metodo resta de dos numeros.

--- supercalculadora.py
+++ supercalculadora.py
@@ -1,3 +1,6 @@
 class Supercalculadora:
     def sumar( self, a, b):
         return a + b
+
+    def restar( self, a, b):
+        return 2
--- test_supercalculadora.py
+++ test_supercalculadora.py
@@ -18,5 +18,8 @@ class TestsSupercalculadora( unittest.TestCase):
     def test_sumar_propiedad_conmutativa(self):
         self.failUnlessEqual(self.calc.sumar(5, 7), self.calc.sumar(7, 5))
 
+    def test_restar_5_y_3( self):
+        self.failUnlessEqual(2, self.calc.restar(5, 3))
+
 if __name__ == "__main__":
     unittest.main()
