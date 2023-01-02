ee3ef3eb1fad54a5689b08cb52aa3cc8e3090673
Orlando Jimenez <orlando.jmz13@gmail.com>
2023-01-02 12:18:51 -0500

    Primer test suma de dos numeros.

---
+++ test_supercalculadora.py
@@ -0,0 +1,10 @@
+import unittest
+import supercalculadora
+
+class TestsSupercalculadora( unittest.TestCase):
+    def test_sumar_2_y_2(self):
+        calc = supercalculadora.Supercalculadora()
+        self.failUnlessEqual(4, calc.sumar(2, 2))
+
+if __name__ == "__main__":
+    unittest.main()
