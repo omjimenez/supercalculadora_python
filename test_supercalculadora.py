84fc9c55ae76b4e7603594fb99c9f99914a7f33a
Orlando Jimenez <orlando.jmz13@gmail.com>
2023-01-02 13:49:18 -0500

    Se refactoriza para eliminar duplicidad del metodo calc.sumar.

--- test_supercalculadora.py
+++ test_supercalculadora.py
@@ -2,13 +2,18 @@ import unittest
 import supercalculadora
 
 class TestsSupercalculadora( unittest.TestCase):
+
+    def setUp(self):
+        self.calc = supercalculadora.Supercalculadora()
+
+    def tearDown(self):
+        pass
+
     def test_sumar_2_y_2(self):
-        calc = supercalculadora.Supercalculadora()
-        self.failUnlessEqual(4, calc.sumar(2, 2))
+        self.failUnlessEqual(4, self.calc.sumar(2, 2))
     
     def test_sumar_5_y_7(self):
-        calc = supercalculadora.Supercalculadora()
-        self.failUnlessEqual(12, calc.sumar(5, 7))
+        self.failUnlessEqual(12, self.calc.sumar(5, 7))
 
 if __name__ == "__main__":
     unittest.main()
