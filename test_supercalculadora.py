13aa7095994a897be0e1bc1a434cc9143503a0ce
Orlando Jimenez <orlando.jmz13@gmail.com>
2023-01-02 13:15:43 -0500

    Se agrega nuevo test.

--- test_supercalculadora.py
+++ test_supercalculadora.py
@@ -5,6 +5,10 @@ class TestsSupercalculadora( unittest.TestCase):
     def test_sumar_2_y_2(self):
         calc = supercalculadora.Supercalculadora()
         self.failUnlessEqual(4, calc.sumar(2, 2))
+    
+    def test_sumar_5_y_7(self):
+        calc = supercalculadora.Supercalculadora()
+        self.failUnlessEqual(12, calc.sumar(5, 7))
 
 if __name__ == "__main__":
     unittest.main()
